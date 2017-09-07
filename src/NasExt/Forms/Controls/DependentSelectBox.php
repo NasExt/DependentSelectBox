046dcea33332e378640e043d4e05dad43e6a76f8
Aleš <aleswita+githubcom@gmail.com>
2017-09-07 09:03:36 +0200

    Add files via upload

--- src/NasExt/Forms/Controls/DependentSelectBox.php
+++ src/NasExt/Forms/Controls/DependentSelectBox.php
@@ -6,7 +6,7 @@
  * Copyright (c) 2013 Dusan Hudak (http://dusan-hudak.com)
  *
  * For the full copyright and license information, please view
- * the file license.txt that was distributed with this source code.
+ * the file license.md that was distributed with this source code.
  */
 
 namespace NasExt\Forms\Controls;
