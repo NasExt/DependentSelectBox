6f84397ff4e282636d3c6744e575bc0f6c391f6f
Aleš <aleswita+githubcom@gmail.com>
2017-09-11 13:20:02 +0200

    Update bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -9,21 +9,21 @@
  * the file license.md that was distributed with this source code.
  */
 
-if (@!include __DIR__ . '/../../../vendor/autoload.php') {
-//if (@!include __DIR__ . '/../vendor/autoload.php') {
+//if (@!include __DIR__ . '/../../../vendor/autoload.php') {
+if (@!include __DIR__ . '/../vendor/autoload.php') {
 	echo 'Install Nette Tester using `composer install`';
 	exit(1);
 }
 
 
 
-require_once __DIR__ . '/../src/DependentTrait.php';
+/*require_once __DIR__ . '/../src/DependentTrait.php';
 require_once __DIR__ . '/../src/Controls/DependentMultiSelectBox.php';
 require_once __DIR__ . '/../src/Controls/DependentSelectBox.php';
 require_once __DIR__ . '/../src/Extension.php';
 require_once __DIR__ . '/../src/DependentData.php';
 require_once __DIR__ . '/../src/Exceptions.php';
-require_once __DIR__ . '/../src/Controls/BackCompatibility.php';
+require_once __DIR__ . '/../src/Controls/BackCompatibility.php'; */
 
 require_once __DIR__ . '/app/presenters/BasePresenter.php';
 require_once __DIR__ . '/app/router/Router.php';
